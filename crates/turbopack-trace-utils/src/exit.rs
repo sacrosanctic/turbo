740eba3313dd1d0b1d11a7d6e270efff34d3f52e
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2024-06-11 19:52:59 -0700

    Rename ExitHandlerMock to ExitReceiver, add docs
    This API is useful outside of mocks.

--- crates/turbopack-trace-utils/src/exit.rs
+++ crates/turbopack-trace-utils/src/exit.rs
@@ -33,7 +33,7 @@ impl<T: Send + 'static> ExitGuard<T> {
     }
 }
 
-type ExitFuture = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
+type BoxExitFuture = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
 
 /// The singular global ExitHandler. This is primarily used to ensure
 /// `ExitHandler::listen` is only called once.
@@ -41,17 +41,29 @@ type ExitFuture = Pin<Box<dyn Future<Output = ()> + Send + 'static>>;
 /// The global handler is intentionally not exposed, so that APIs that depend on
 /// exit behavior are required to take the `ExitHandler`. This ensures that the
 /// `ExitHandler` is configured before these APIs are run, and that these
-/// consumers can be used with a mock instead.
+/// consumers can be used with a callback (e.g. a mock) instead.
 static GLOBAL_EXIT_HANDLER: OnceLock<Arc<ExitHandler>> = OnceLock::new();
 
 pub struct ExitHandler {
-    tx: mpsc::UnboundedSender<ExitFuture>,
+    tx: mpsc::UnboundedSender<BoxExitFuture>,
 }
 
 impl ExitHandler {
+    /// Waits for `SIGINT` using [`tokio::signal::ctrl_c`], and exits the
+    /// process with exit code `0` after running any futures scheduled with
+    /// [`ExitHandler::on_exit`].
+    ///
+    /// As this uses global process signals, this must only be called once, and
+    /// will panic if called multiple times. Use this when you own the
+    /// process (e.g. `turbopack-cli`).
+    ///
+    /// If you don't own the process (e.g. you're called as a library, such as
+    /// in `next-swc`), use [`ExitHandler::new_trigger`] instead.
+    ///
+    /// This may listen for other signals, like `SIGTERM` or `SIGPIPE` in the
+    /// future.
     pub fn listen() -> &'static Arc<ExitHandler> {
-        let (tx, mut rx) = mpsc::unbounded_channel();
-        let handler = Arc::new(ExitHandler { tx });
+        let (handler, receiver) = Self::new_receiver();
         if GLOBAL_EXIT_HANDLER.set(handler).is_err() {
             panic!("ExitHandler::listen must only be called once");
         }
@@ -59,17 +71,31 @@ impl ExitHandler {
             tokio::signal::ctrl_c()
                 .await
                 .expect("failed to set ctrl_c handler");
-            run_all_futures(&mut rx).await;
+            receiver.run_exit_handler().await;
             std::process::exit(0);
         });
         GLOBAL_EXIT_HANDLER.get().expect("value is set")
     }
 
-    pub fn mock() -> (Arc<ExitHandler>, ExitHandlerMocks) {
+    /// Creates an [`ExitHandler`] that can be manually controlled with an
+    /// [`ExitReceiver`].
+    ///
+    /// This does not actually exit the process or listen for any signals. If
+    /// you'd like that behavior, use [`ExitHandler::listen`].
+    ///
+    /// Because this API has no global side-effects and can be called many times
+    /// within the same process, it is possible to use it to provide a mock
+    /// [`ExitHandler`] inside unit tests.
+    pub fn new_receiver() -> (Arc<ExitHandler>, ExitReceiver) {
         let (tx, rx) = mpsc::unbounded_channel();
-        (Arc::new(ExitHandler { tx }), ExitHandlerMocks { rx })
+        (Arc::new(ExitHandler { tx }), ExitReceiver { rx })
     }
 
+    /// Register this given [`Future`] to run upon process exit.
+    ///
+    /// As there are many ways for a process be killed that are outside of a
+    /// process's own control (e.g. `SIGKILL` or `SIGSEGV`), this API is
+    /// provided on a best-effort basis.
     pub fn on_exit(&self, fut: impl Future<Output = ()> + Send + 'static) {
         // realistically, this error case can only happen with a mock
         self.tx
@@ -78,33 +104,36 @@ impl ExitHandler {
     }
 }
 
-pub struct ExitHandlerMocks {
-    rx: mpsc::UnboundedReceiver<ExitFuture>,
+/// Provides a way to run futures scheduled with an [`ExitHandler`].
+pub struct ExitReceiver {
+    rx: mpsc::UnboundedReceiver<BoxExitFuture>,
 }
 
-impl ExitHandlerMocks {
-    pub async fn mock_exit(mut self) {
-        run_all_futures(&mut self.rx).await
-    }
-}
-
-pub async fn run_all_futures(rx: &mut mpsc::UnboundedReceiver<ExitFuture>) {
-    let mut set = JoinSet::new();
-    while let Ok(fut) = rx.try_recv() {
-        set.spawn(fut);
-    }
-    loop {
-        select! {
-            Some(fut) = rx.recv() => {
-                set.spawn(fut);
-            },
-            val = set.join_next() => {
-                match val {
-                    Some(Ok(())) => {}
-                    Some(Err(_)) => panic!("ExitHandler future panicked!"),
-                    None => return,
-                }
-            },
+impl ExitReceiver {
+    /// Call this when the process exits to run the futures scheduled via
+    /// [`ExitHandler::on_exit`].
+    ///
+    /// As this is intended to be used in a library context, this does not exit
+    /// the process. It is expected that the process will not exit until
+    /// this async method finishes executing.
+    pub async fn run_exit_handler(mut self) {
+        let mut set = JoinSet::new();
+        while let Ok(fut) = self.rx.try_recv() {
+            set.spawn(fut);
+        }
+        loop {
+            select! {
+                Some(fut) = self.rx.recv() => {
+                    set.spawn(fut);
+                },
+                val = set.join_next() => {
+                    match val {
+                        Some(Ok(())) => {}
+                        Some(Err(_)) => panic!("ExitHandler future panicked!"),
+                        None => return,
+                    }
+                },
+            }
         }
     }
 }
